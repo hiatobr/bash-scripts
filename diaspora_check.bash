c0bb1f686164120a8ae671b3ada33352211860a2
Iuri Guilherme <iuri@riseup.net>
2014-08-12 03:13:28 +0200

    Consertado link de novo

--- diaspora_check.bash
+++ diaspora_check.bash
@@ -1,1 +1,15 @@
-/home/hiato/bin/diaspora_check.bash
+#!/bin/bash
+
+COMMAND=`nmap -p 3000 127.0.0.1 | grep -e 'open'`
+MESSAGE="3000/tcp open  ppp"
+DAEMON="/etc/init.d/diaspora restart"
+LOG="/home/hiato/var/log/diaspora_check.log"
+
+
+if [ "${COMMAND}" == "${MESSAGE}" ]
+then
+	echo "[${0}] (`date`): Porta 3000 escutando normalmente." 1>> "${LOG}"
+else
+	${DAEMON}
+fi
+
